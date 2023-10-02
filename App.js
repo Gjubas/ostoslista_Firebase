1a85656e2b3461958df2e0cdde8a33bdd36fc9a0
Oliver Roman <oltsu5@hotmail.com>
2023-10-02 04:03:06 +0300

    dog code

--- App.js
+++ App.js
@@ -1,11 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import firebase from "firebase/app";
+import { firebaseConfig } from "./firebaseConfig";
+import "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 export default function App() {
+  const [productName, setProductName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [shoppingList, setShoppingList] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // Firebase is initialized and user is authenticated (or null if not authenticated)
+        loadShoppingList();
+      }
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  const addItem = async () => {
+    if (productName && amount) {
+      try {
+        await db.collection("items").add({
+          name: productName,
+          amount: amount,
+        });
+        // Clear input fields
+        setProductName("");
+        setAmount("");
+        // Reload shopping list
+        loadShoppingList();
+      } catch (error) {
+        console.error("Error adding item: ", error);
+      }
+    }
+  };
+
+  const loadShoppingList = async () => {
+    try {
+      const itemsSnapshot = await db.collection("items").get();
+      const items = itemsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setShoppingList(items);
+    } catch (error) {
+      console.error("Error loading shopping list: ", error);
+    }
+  };
+
+  const removeItem = async (id) => {
+    try {
+      await db.collection("items").doc(id).delete();
+      // Reload shopping list after deleting an item
+      loadShoppingList();
+    } catch (error) {
+      console.error("Error removing item: ", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Shopping List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Product"
+          value={productName}
+          onChangeText={(text) => setProductName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Amount"
+          value={amount}
+          onChangeText={(text) => setAmount(text)}
+        />
+        <Button title="Save" onPress={addItem} />
+      </View>
+
+      <FlatList
+        data={shoppingList}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.listItems}>
+            <Text style={styles.listItem}>{item.name}</Text>
+            <Text style={styles.listItem}>{item.amount}</Text>
+            <TouchableOpacity onPress={() => removeItem(item.id)}>
+              <Text style={styles.deleteButton}>Bought</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +112,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  heading: {
+    marginTop: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  inputContainer: {
+    width: "75%",
+  },
+  input: {
+    height: 35,
+    borderColor: "black",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+  listItems: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  listItem: {
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  deleteButton: {
+    color: "red",
+    marginLeft: 10,
   },
 });
---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvpF3mgCVmVuFP66uojtKvHfdGOA8zcdU",
+  authDomain: "ostoslistafirebase-72c83.firebaseapp.com",
+  projectId: "ostoslistafirebase-72c83",
+  storageBucket: "ostoslistafirebase-72c83.appspot.com",
+  messagingSenderId: "758904251217",
+  appId: "1:758904251217:web:eb513f1a42a48dd2ee2970",
+  measurementId: "G-SSVRSYYBFR",
+};
